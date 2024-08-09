9face7500029811f8b9765f16408c7fe498b974e
gaganevomorf <gagan@evomorf.com>
2024-08-09 16:15:36 +0530

    allow origin

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "http://localhost:5000",
+            value: "*",
           }, // replace this your actual origin
           {
             key: "Access-Control-Allow-Methods",
